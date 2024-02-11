720bffb6903e152681c54f36121186bf5630258b
gorbachev <lalalala040605@gmail.com>
2024-02-12 00:29:31 +0400

    2 функции

--- feature/Task2/str_func.py
+++ feature/Task2/str_func.py
@@ -1,3 +1,18 @@
+"""Строка ввода"""
 stroka = input()
-stroka1 = stroka.upper()
-print(stroka1)
+
+def zagl(stroka):
+    """Строка делает буквы заглавными"""
+    stroka1 = stroka.upper()
+    """Вывод"""
+    return stroka1
+
+def zagl1(stroka):
+    """
+    Функция делает заглавными первые буквы каждого слова в строке.
+    """
+    return ' '.join(word.capitalize() for word in stroka.split())
+
+# Вызываем функции с передачей аргумента stroka
+print(zagl(stroka))
+print(zagl1(stroka))
